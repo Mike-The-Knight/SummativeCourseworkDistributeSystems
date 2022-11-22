58ccf372792d20e03b1d1f4dfabb6954117967a4
mw01180 <mw01180@surrey.ac.uk>
2022-11-22 14:55:21 +0000

    Formatting

---
+++ Paxos.ex
@@ -0,0 +1,23 @@
+def module Paxos do
+    def start (name, participants) do
+    
+    
+    end
+
+    def propose (pid, inst, value, t) do
+        state = receive do
+            (:decision, v) ->
+
+            (:abort) ->
+
+            (:timeout) ->
+
+            
+        end
+    end
+
+    def get_decision(pid, inst, t) do
+
+    end
+        
+end
